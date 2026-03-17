dc284f00ba74ae332f4dbd46ccb0617bfc1a42aa
Miguel Bustos <miguelb.bustos1907@gmail.com>
2026-03-17 14:21:59 -0500

    Verificar palindromo

--- Ejercicios/src/App.java
+++ Ejercicios/src/App.java
@@ -1,5 +1,23 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        String palabra = "oso";
+        boolean esPalindromo = false;
+
+        for (int i = 0; i < palabra.length(); i++) {
+            for (int j = palabra.length() - 1; j >= 0; j--) {
+                if (palabra.charAt(i) == palabra.charAt(j)) {
+                    esPalindromo = true;
+                } else {
+                    esPalindromo = false;
+                }
+            }
+        }
+
+        if (esPalindromo) {
+            System.out.println("La palabra es palindromo");
+        } else {
+            System.out.println("La palabra no es palindromo");
+        }
 
     }
 }
