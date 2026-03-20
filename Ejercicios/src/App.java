7c08c9b704c322cb02e0a1ec3ec1a8715da1e6a7
Miguel Bustos <miguelb.bustos1907@gmail.com>
2026-03-19 21:53:47 -0500

    Crear clase de estudiante

--- Ejercicios/src/App.java
+++ Ejercicios/src/App.java
@@ -1,30 +1,44 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        String palabra = "oso";
-        boolean esPalindromo = false;
+        /*
+         * String palabra = "oso";
+         * boolean esPalindromo = false;
+         * 
+         * for (int i = 0; i < palabra.length(); i++) {
+         * for (int j = palabra.length() - 1; j >= 0; j--) {
+         * if (palabra.charAt(i) == palabra.charAt(j)) {
+         * esPalindromo = true;
+         * } else {
+         * esPalindromo = false;
+         * }
+         * }
+         * }
+         * if (esPalindromo) {
+         * System.out.println("La palabra es palindromo");
+         * } else {
+         * System.out.println("La palabra no es palindromo");
+         * }
+         * System.out.println("Hola");
+         * 
+         * int[] numeros = { 5, 12, 3, 20 };
+         * for (int i : numeros) {
+         * if (i > 10) {
+         * System.out.println(i);
+         * }
+         * }
+         */
 
-        for (int i = 0; i < palabra.length(); i++) {
-            for (int j = palabra.length() - 1; j >= 0; j--) {
-                if (palabra.charAt(i) == palabra.charAt(j)) {
-                    esPalindromo = true;
-                } else {
-                    esPalindromo = false;
-                }
-            }
-        }
+        Estudiante estudiante1 = new Estudiante("Miguel Ángel Bustos Malagón");
 
-        if (esPalindromo) {
-            System.out.println("La palabra es palindromo");
-        } else {
-            System.out.println("La palabra no es palindromo");
-        }
-        System.out.println("Hola");
+        List<Float> notas = new ArrayList<>(Arrays.asList(3.0f, 4.2f, 2.4f));
 
-        int[] numeros = { 5, 12, 3, 20 };
-        for (int i : numeros) {
-            if (i > 10) {
-                System.out.println(i);
-            }
-        }
+        estudiante1.agregarNotas(notas);
+
+        estudiante1.calcularPromedio();
+
     }
 }
---
+++ Ejercicios/src/Estudiante.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Estudiante {
+    private String nombreEstudiante;
+    private List<Float> notas = new ArrayList<>();
+
+    public Estudiante(String nombre) {
+        this.nombreEstudiante = nombre;
+    }
+
+    public void agregarNotas(List<Float> nota) {
+        for (Float n : nota) {
+            notas.add(n);
+        }
+    }
+
+    public void calcularPromedio() {
+
+        if (notas.size() < 2) {
+            System.out.println("Error: debe haber al menos 2 notas");
+            return;
+        }
+
+        double promedio = notas.stream().mapToDouble(n -> n).average().orElse(0);
+
+        if (promedio > 3.0) {
+            System.out.printf("El estudiante: %s \n Promedio: %.1f \n Resultado: Aprobado", nombreEstudiante, promedio);
+        } else {
+            System.out.printf("El estudiante: %s \n Promedio: %.1f \n Resultado: Reprobado", nombreEstudiante,
+                    promedio);
+        }
+    }
+
+}
